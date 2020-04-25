dfe848f71b32b03fb6c757602342e8bd92eaa82c
LilianLourenco <lylianlourenco@gmail.com>
2020-04-25 13:42:08 +0000

    search trying

--- mobdev-ca2-2020323/src/app/pages/episodes/episodes.page.ts
+++ mobdev-ca2-2020323/src/app/pages/episodes/episodes.page.ts
@@ -11,13 +11,16 @@ import { ApiService } from    '../../services/api.service';
 export class EpisodesPage implements OnInit {
 
     episodes: Observable<any>;
-
+    episodeList: any[];
+    searchEpisodeList: any[];
     constructor(private router: Router, private api: ApiService) { }
 
     ngOnInit() {
         this.episodes = this.api.getEpisodes();
         this.episodes.subscribe(data => {
         console.log('my data', data);
+        this.episodeList;
+        this.searchEpisodeList;
         });
     }
 
@@ -25,4 +28,40 @@ export class EpisodesPage implements OnInit {
         let episodeId = episode.episode_id;        
         this.router.navigateByUrl(`/tabs/episodes/${episodeId}`);
     }
+
+
+
+
+initializeItems(): void {
+  this.episodeList = this.searchEpisodeList;
+        this.router.navigateByUrl(`/tabs/episodes/${EpisodesPage}`);
+}
+
+
+
+
+filterList(evt) {
+  this.initializeItems();
+
+  const searchTerm = evt.srcElement.value;
+
+  if (!searchTerm) {
+    return;
+  }
+
+  this.episodeList = this.episodeList.filter(currentEpisode => {
+    if (currentEpisode.episode && searchTerm) {
+      if (currentEpisode.episode.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+        return true;
+      }
+      return false;
+    }
+  });
+}
+
+
+
+
+
+
 }
